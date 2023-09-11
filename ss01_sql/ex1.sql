40b347e32f10bf51bc21a17a6ea5d25dcad0aa3c
Quang-Truong-2001 <quangtruong2112001@gmail.com>
2023-09-11 10:42:33 +0700

    submit_excercise_ss01

---
+++ ss01_sql/ex1.sql
@@ -0,0 +1,12 @@
+create database ss01;
+use ss01;
+create table student(
+	id int primary key auto_increment,
+    student_name varchar(50)
+);
+create table teacher(
+	id int primary key auto_increment,
+	teacher_name varchar(50),
+    age int,
+    country varchar(50)
+);
