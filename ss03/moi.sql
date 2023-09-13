18798e97809e130d9f719b4998be717d43f1cd6a
Quang-Truong-2001 <quangtruong2112001@gmail.com>
2023-09-13 21:11:31 +0700

    submit_ss03

--- ss03/moi.sql
+++ ss03/moi.sql
@@ -30,12 +30,11 @@ group by khach_hang.ma_khach_hang;
 --  Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)
 --  cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng 
 --  cũng phải hiển thị ra).
-select k.ma_khach_hang, k.ho_ten, l.ten_loai_khach, h.ma_hop_dong, d.ten_dich_vu, h.ngay_lam_hop_dong,
-sum(d.chi_phi_thue+ifnull(hc.so_luong,0)*ifnull(dk.gia,0)) as total_price
+select k.ma_khach_hang, k.ho_ten, l.ten_loai_khach, h.ma_hop_dong, d.ten_dich_vu, h.ngay_lam_hop_dong,(d.chi_phi_thue+ifnull(hc.so_luong,0)*ifnull(dk.gia,0)) as total_price
 from khach_hang k
 join loai_khach l
 on	k.ma_loai_khach=l.ma_loai_khach
-inner join hop_dong h
+left join hop_dong h
 on k.ma_khach_hang=h.ma_khach_hang
 left join dich_vu d
 on h.ma_dich_vu=d.ma_dich_vu
